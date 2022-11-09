46e5dd32a6acb39e84d0b18939dcc734548f41c2
gabrielbartolome <84766332+gabrielbartolome@users.noreply.github.com>
2022-11-09 18:35:01 +0100

    Anfang Aufgaben Greisch

---
+++ auditlogserver/src/main/java/edu/hm/dako/auditlogserver/gui/ALServerGUIInterface.java
@@ -0,0 +1,32 @@
+package edu.hm.dako.auditlogserver.gui;
+
+import edu.hm.dako.auditlogserver.ServerStartData;
+
+/**
+ * Interface, das der AuditLog ServerGUI bereitstehen muss
+ *
+ * @author Gabriel Bartolome
+ */
+public interface ALServerGUIInterface {
+    /**
+     * shows server start information in the GUI, e.g. the start time
+     *
+     * @param data start data of the server
+     */
+    //void showStartData(ServerStartData data);
+
+    /**
+     * increases the number of logged in clients
+     */
+    void increaseNumberOfLoggedInClients();
+
+    /**
+     * decreases the number of logged in clients
+     */
+    void decreaseNumberOfLoggedInClients();
+
+    /**
+     * increases the number of incoming requests
+     */
+    void increaseNumberOfRequests();
+}
