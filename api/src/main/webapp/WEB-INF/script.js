551469a21d200eb960e5c691285750dca838caf6
OskarHM <oskar-gruss@t-online.de>
2022-12-19 23:17:52 +0100

    Funktionierender API Call über newtry.html

--- api/src/main/webapp/WEB-INF/script.js
+++ api/src/main/webapp/WEB-INF/script.js
@@ -1,24 +1,16 @@
 // api url
 const api_url =
-    "https://localhost:5001/api/ansprechpartner";
+    "http://localhost:8080/api/pdus/types";
 
 // Defining async function
 async function getapi(url) {
 
     // Storing response
-   // console.log(fetch(url , { method: "GET", mode: "no-cors" }).toString());
-    let response  = await fetch(url); //mode: "no-cors"
-    console.log( response);
-    let data =await response.json();
-    //console.log(JSON.parse(response));
-
+    const response = await fetch(url);
 
     // Storing data in form of JSON
-    var test1 = '{"Heh": 2, "meh": 3}';
-    console.log(test1);
-    var test11 =JSON.parse(test1);
-    console.log(test11);
-    //var data = JSON.stringify(response);
+    var data = await response.json();
+    console.log(data);
     if (response) {
         hideloader();
     }
@@ -33,55 +25,27 @@ function hideloader() {
 }
 // Function to define innerHTML for HTML table
 function show(data) {
+    console.log(data.login);
     let tab =
         `<tr>
-		<th>Logout</th>
-		<th>Chat</th>
-		<th>Finish</th>
-		<th>Login</th>
-		<th>Undefined</th>
-
-		</tr>`;
+          <th>Undefined</th>
+          <th>Login</th>
+          <th>Chat</th>
+          <th>Finish</th>
+         </tr>`;
 
     // Loop to access all rows
-    for (let r of data.list) {
-        tab += `<tr>
-	<td>${r.logout} </td>
-	<td>${r.chat}</td>
-	<td>${r.finish}</td>
-	<td>${r.login}</td>	
-	<td>${r.undefined}</td>		
-	
+    //for (let datas of data) {
+        //undefined, login, logout, chat, finish
+        tab += `<tr> 
+    <td>${data.undefined} </td>
+    <td>${data.login} </td>
+    <td>${data.logout} </td>
+    <td>${data.chat} </td>
+    <td>${data.finish} </td>    
 </tr>`;
-    }
+    //}
     // Setting innerHTML as tab variable
     document.getElementById("employees").innerHTML = tab;
 }
-
-/*fetch("http://localhost:8080/api/pdus/types", { method: "GET", mode: "no-cors" })
-    .then(function(response){
-        return JSON.parse(response.toString());
-      //  return response.json();
-
-    })
-    .then(function(pduTypes){
-        let placeholder = document.querySelector("#data-output");
-        let out = "";
-        for(let pduType of pduTypes){
-            out += `
-			<tr>
-				<td>${pduType.logout}'> </td>
-				<td>${pduType.chat}'> </td>
-				<td>${pduType.finish}</td>
-				<td>${pduType.login}</td>
-				<td>${pduType.undefined}</td>
-			</tr>
-		`;
-        }
-
-        placeholder.innerHTML = out;
-    });
-
-///
-*/
 
